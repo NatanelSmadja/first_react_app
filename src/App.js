5b2038c3f4bf369b6626037cba560d66b517b3d8
Natanel Smadja <N.Smad@college.labs>
2024-04-08 10:04:54 +0300

    adding a toastify alert

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import  {ToastContainer, toast} from 'react-toastify'
 const App = () => {
+
+    const handleClick = () =>{
+        toast.success('Hello from ReactJS')
+    }
+
   return (
-    <div>App</div>
+    <>
+    <ToastContainer />
+        <div>App</div>
+        <button onClick={handleClick} className='btn btn-warning btn-lg' >CLICK ME</button>
+    </>
   )
 }
 
